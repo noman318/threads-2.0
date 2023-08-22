22f4550cc60f5b68c3fb70768ad6ab30b06a9ba6
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2023-08-22 19:59:38 +0530

    Working on creating API

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,13 +1,27 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
 import React from "react";
 
-const Onboarding = () => {
+const Onboarding = async () => {
+  const user = await currentUser();
+  const userInfo = {};
+  const userData = {
+    id: user?.id,
+    objectId: userInfo._id,
+    username: userInfo?.username || user?.username || "",
+    name: userInfo?.name || user?.firstName,
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
   return (
     <main className="flex mx-auto max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
       <p className="mt-3 text-base-regular text-light-2">
         Complete your profile now to use Threads
       </p>
-      <section className="mt-9 bg-dark-2 p-10"></section>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
