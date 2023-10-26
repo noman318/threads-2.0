5671d8690a1d31fedb5a7731df5051fdbfbb7a90
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2023-10-26 19:17:59 +0530

    Activity section completed

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { fetchUser, getActivity } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  // console.log("userInfo", userInfo);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo?._id);
+  // console.log("activity", activity);
   return (
     <section>
       <h1 className="head-text">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity?.length > 0 ? (
+          activity.map((data) => (
+            <Link key={data?.id} href={`/thread/${data?.parentId}`}>
+              <article className="activity-card">
+                <Image
+                  src={data.author.image}
+                  alt="Profile Image"
+                  className="rounded-full object-cover"
+                  height={30}
+                  width={30}
+                />
+                <p className="!text-small-regular text-light-1">
+                  <span className="mr-1 text-primary-500">
+                    {data.author.name}
+                  </span>{" "}
+                  replied to your thread.
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   );
 };
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -127,3 +127,22 @@ export async function fetchUsers({
     throw new Error(`Error fethching All Users ${error}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+    const userThreads = await Thread.find({ author: userId });
+    const childThreadsIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadsIds },
+      author: { $ne: userId },
+    }).populate({ path: "author", model: User, select: "name image _id" });
+    return replies;
+  } catch (error: any) {
+    console.log("errorInGettingActivity", error);
+    throw new Error(`Error fethching Activities ${error}`);
+  }
+}
